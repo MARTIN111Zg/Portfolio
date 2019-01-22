703fb43b456d745399117f98850054a4dd0abe91
Martin Gorupić <37804840+MARTIN111Zg@users.noreply.github.com>
2019-01-22 05:03:59 +0100

    Portfolio

---
+++ src/js/index.js
@@ -0,0 +1,71 @@
+import "../sass/main.scss";
+import TweenMax from "gsap/TweenMax";
+
+const h2 = document.querySelector(".overlay__h2");
+const overlay = document.querySelector(".overlay");
+const nav = document.querySelector(".nav__wrapper");
+const left = document.querySelector(".hero__container--left");
+const right = document.querySelector(".hero__container--right");
+const para = document.querySelector(".text__container__p");
+
+
+TweenMax.to(h2, 1, {
+    opacity: 1,
+
+    x: "0",
+    delay: 1
+});
+TweenMax.to(h2, 2, {
+    opacity: 0,
+    scale: 0,
+    ease: Expo.easeInOut,
+    delay: 3
+});
+TweenMax.to(overlay, 1.5, {
+    height: "0",
+    ease: Expo.easeInOut,
+    delay: 5.5
+});
+TweenMax.to(nav, 2, {
+    opacity: 1,
+    scale: 1,
+    delay: 7.5
+});
+TweenMax.staggerFrom(".nav__wrapper ul li", 2, {
+    opacity: 0,
+    delay: 8,
+    y: 50,
+    ease: Expo.easeInOut
+}, 0.3);
+TweenMax.staggerFrom(".user__icons  li", 2, {
+    opacity: 0,
+    delay: 11,
+    y: 30,
+    ease: Expo.easeInOut
+}, 0.3);
+TweenMax.to(left, 3, {
+    opacity: 1,
+    scale: 1,
+    ease: Expo.easeInOut,
+    x: "0",
+    delay: 9
+});
+TweenMax.to(right, 3, {
+    opacity: 1,
+    scale: 1,
+    ease: Expo.easeInOut,
+    x: "0",
+    delay: 9
+});
+TweenMax.staggerFrom(".contact__info  a", 1.5, {
+    opacity: 0,
+    delay: 13,
+    y: 30,
+    ease: Expo.easeInOut
+}, 0.3);
+TweenMax.to(para, 2, {
+    opacity: 1,
+    scale: 1,
+    ease: Expo.easeInOut,
+    delay: 15
+});
